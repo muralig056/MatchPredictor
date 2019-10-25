0d6db74766b947acba83984636b15fc27b6494d8
Murali <muralig056@gmail.com>
2019-10-25 15:54:55 +0530

    Added unit tests

--- MatchPredictor.Tests/Models/PlayerTests.cs
+++ MatchPredictor.Tests/Models/PlayerTests.cs
@@ -1,5 +1,6 @@
 ﻿using MatchPredictor.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace MatchPredictor.Tests.Models
@@ -27,15 +28,50 @@ namespace MatchPredictor.Tests.Models
         }
 
         [TestMethod]
-        public void UpdatePlayers_ScoredOddRuns_StrikeChangedTest()
+        public void GetWeightedRandomNumber_GivenRandomNumber_ReturnsRunsBasedOnProbability()
         {
             var probability = new int[] { 10, 20, 20, 5, 15, 5, 10, 15 };
             var target = new Player(1, "TestPLayer", probability);
 
-
             int runs = target.GetWeightedRandomNumber(60);
 
             Assert.AreEqual(4, runs);
+        }
+
+        [TestMethod]
+        public void GetBallResult_100Balls_PlayerScoreEqualsTotalScore()
+        {
+            List<int> runsFor100Balls = new List<int>();
+            var player = new Player(1, "Test player", new int[] { 5, 30, 25, 10, 15, 1, 9, 5 });
+            for (var i = 0; i < 100; i++)
+            {
+                runsFor100Balls.Add(player.GetBallResult());
+            }
+
+            var runsPossibilities = player.RunPossibilities;
+
+            var runsProbability = new List<int>();
+            for (var i = 0; i < runsPossibilities.Length; i++)
+            {
+                var sampleDistribution = GetSampleDistribution(runsFor100Balls, runsPossibilities[i]);
+                runsProbability.Add(sampleDistribution);
+            }
+
+            int totalRuns = 0;
+            for (var i = 0; i < runsFor100Balls.Count; i++)
+                totalRuns += runsFor100Balls[i] != -1 ? runsFor100Balls[i] : 0;
+            Assert.AreEqual(player.Score, totalRuns);
+        }
+
+        private int GetSampleDistribution(List<int> runsFor100Balls, int probableRun)
+        {
+            var noOfOccurences = 0;
+            for (var i = 0; i < runsFor100Balls.Count; i++)
+            {
+                if (runsFor100Balls[i] == probableRun)
+                    noOfOccurences++;
+            }
+            return noOfOccurences;
         }
     }
 }
